0c59cd70516c2d8cc48309f6efbe5837cc4e14f7
Thirak <chuayjan.thirak@gmail.com>
2021-08-03 22:09:27 +0700

    first commit

---
+++ contracts/MyToken.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.6.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply) public ERC20("Rak Coin", "RKC") {
+        _mint(msg.sender, initialSupply);
+        _setupDecimals(0);
+    }
+}
